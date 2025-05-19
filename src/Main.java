028d270df392a0eaeb540187e36a6e625ed71c91
sao-v <saovarun0406@outlook.com>
2025-05-19 23:55:49 +0530

    The Beginning

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ans={0,0};
+        int size = nums.length;
+        for(int i=0;i<size-1;i++){
+            for(int j=i+1;j<size;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int arr[]={1,2,3,4,5,6,7};
+        int target = 5;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
